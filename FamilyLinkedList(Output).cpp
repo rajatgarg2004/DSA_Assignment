0dd236fe6f9ba31d033bcbcbdad26acf838b3155
rajatgarg2004 <96770616+rajatgarg2004@users.noreply.github.com>
2022-09-24 23:32:06 +0530

    Create FamilyLinkedList(Output).cpp

---
+++ FamilyLinkedList(Output).cpp
@@ -0,0 +1,12 @@
+Enter number of members in family:- 5 
+Enter name of family member with their age:- 
+Enter details of member:- Dad 36
+Enter details of member:- Mom 34   
+Enter details of member:- Son 20
+Enter details of member:- Grandpa 52
+Enter details of member:- Grandma 50
+Dad 36
+Mom 34
+Son 20
+Grandpa 52
+Grandma 50
