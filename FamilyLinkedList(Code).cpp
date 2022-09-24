479c6ee88d7e3052225a022b41f987ca4d2ea3ee
rajatgarg2004 <96770616+rajatgarg2004@users.noreply.github.com>
2022-09-25 00:00:55 +0530

    Update FamilyLinkedList(Code).cpp

--- FamilyLinkedList(Code).cpp
+++ FamilyLinkedList(Code).cpp
@@ -1,22 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Node
-{
+
+// Class to create a doubly linked list node
+class Node{
     public:
-    string name;
+    string name; 
     int age;
     Node* next;
     Node* prev;
-
-    Node(string name, int age)
-    {
+// Constructor 
+    Node(string name, int age){
         this->name = name;
         this->age = age;
         next = NULL;
         prev = NULL;
     }
 };
-
+// Function for creating a node
 Node* create(string *name, int *age, int members)
 {
     Node* curr=NULL;
@@ -24,10 +24,10 @@ Node* create(string *name, int *age, int members)
     Node* next_ = NULL;
     Node* head = NULL;
     for(int i=0;i<members;i++)
-    {
-        
-        curr = new Node(name[i],age[i]);
-        if(head==NULL)
+    {        
+        curr = new Node(name[i],age[i]);        
+// Checking if linked list is currently empty
+	if(head==NULL)
         {
             head = curr;
         }
@@ -38,10 +38,9 @@ Node* create(string *name, int *age, int members)
         }
         prev_ = curr;
     }
-
     return head;
 }
-
+// Function to add the values of age and name to the current linked list head node
 void add(Node* head, int age, string name)
 {
     Node* nh = new Node(name, age);
@@ -49,7 +48,7 @@ void add(Node* head, int age, string name)
     head->prev = nh;
     head = nh;
 }
-
+// Printing the contents of linked list node
 void print(Node* head)
 {
     while(head!=NULL)
@@ -58,20 +57,21 @@ void print(Node* head)
         head = head->next;
     }
 }
-
+// Main function
 int main()
 {
-    int n;
-    cout<<"Enter number of members in family:- ";
-    cin>>n;
-
-    int *age = new int[n];
+// Variable for number of family members
+    int n;  
+    cout<<"Enter number of members in family: ";
+    cin>>n;	
+// Array for storing ages of family members
+    int *age = new int[n];   
+// Array for storing names of family members
     string *name = new string[n];
-
-    cout<<"Enter name of family member with their age:- "<<endl;
+    cout<<"Enter name of family member along with their age"<<endl;
     for(int i=0;i<n;i++)
     {
-        cout<<"Enter details of member:- ";
+        cout<<"Enter details of member :";
         cin>>name[i]>>age[i];
     }
     Node* head = create(name, age, n);
