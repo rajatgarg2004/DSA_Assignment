a11d6a4995b9183e01465ba3d2e1efb04eedba70
rajatgarg2004 <96770616+rajatgarg2004@users.noreply.github.com>
2022-09-25 00:07:55 +0530

    Update FamilyLinkedList(Output).cpp

--- FamilyLinkedList(Output).cpp
+++ FamilyLinkedList(Output).cpp
@@ -1,12 +1,12 @@
-Enter number of members in family:- 5 
-Enter name of family member with their age:- 
-Enter details of member:- Dad 36
-Enter details of member:- Mom 34   
-Enter details of member:- Son 20
-Enter details of member:- Grandpa 52
-Enter details of member:- Grandma 50
-Dad 36
-Mom 34
-Son 20
-Grandpa 52
-Grandma 50
+Enter number of members in family: 5
+Enter name of family member along with their age
+Enter details of member :Grandfather 52
+Enter details of member :Grandmother 50
+Enter details of member :Father 36
+Enter details of member :Mother 34
+Enter details of member :Son 18
+Grandfather 52
+Grandmother 50
+Father 36
+Mother 34
+Son 18
